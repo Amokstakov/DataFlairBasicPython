ae5bbf1fba6d8f8c393be349aa1871a624ce003b
Amokstakov <amoksyak@uoguelph.ca>
2020-09-25 00:52:50 -0400

    hot fixes

--- YouTubeDownloader/init-letsencrypt.sh
+++ YouTubeDownloader/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(test1.melkey.dev www.test1.melkey.dev)
+domains=(sasha.melkey.dev www.sasha.melkey.dev)
 rsa_key_size=4096
 data_path="./client/data/certbot"
 email="" # Adding a valid address is strongly recommended
